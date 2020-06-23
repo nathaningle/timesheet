1fba7f3a34a50bb2b672439e5d5016e564170e6a
Nathan Ingle <elgni.nahtan@gmail.com>
2020-06-24 06:44:45 +1000

    Add pretty-printer

--- src/Main.hs
+++ src/Main.hs
@@ -13,18 +13,19 @@ Calculate time worked from timesheets.
 module Main where
 
 import           Parse                (parseLines)
-import           WorkDay              (dayBalance)
+import           WorkDay              (ppDayRecord)
 
 import           Data.Attoparsec.Text (endOfInput, parseOnly)
 import           Data.Text            (Text)
+import qualified Data.Text.IO         as TIO
 
 
 sampleInput :: Text
-sampleInput = "2020-01-01\t0700\t1200\t1230\t1500  # Happy New Year  \n\n2020-01-02 8:10  12:10  13:10  16:30\n\n  # Holidays\n"
+sampleInput = "2020-01-01\t0700\t1200\t1230\t1500  # Happy New Year  \n\n2020-01-02 8:10  08:50  09:10  12:10  13:10  14:55  15:10  16:30\n\n  # Holidays\n"
 
 
 main :: IO ()
 main = do
   case parseOnly (parseLines <* endOfInput) sampleInput of
     Left err   -> error err
-    Right recs -> mapM_ (\rec -> print rec >> print (dayBalance rec)) recs
+    Right recs -> mapM_ (TIO.putStrLn . ppDayRecord) recs
--- src/WorkDay.hs
+++ src/WorkDay.hs
@@ -9,15 +9,26 @@ Portability : non-portable
 
 Calculate time worked from timesheets.
 -}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 module WorkDay where
 
 import           Data.Text          (Text)
+import qualified Data.Text          as T
 import           Data.Time.Calendar (Day)
+import           Text.Printf        (printf)
 
 
 -- | Minutes scheduled in a regular working day.  Equal to 7.5 hours.
-scheduledMinutes :: Int
-scheduledMinutes = 450
+regulationMinutes :: Int
+regulationMinutes = 450
+
+
+-- | Show a two-digit 'Int', with a leading zero if necessary.
+ppInt :: Int -> Text
+ppInt x
+  | x < 10 = "0" <> T.pack (show x)
+  | otherwise = T.pack (show x)
 
 
 -- | Time of some day represented as minutes since midnight.
@@ -27,6 +38,11 @@ newtype TimeOfDay = TimeOfDay Int deriving (Eq, Ord, Show)
 -- the first argument is less than the second then the result will be negative.
 minutesDiff :: TimeOfDay -> TimeOfDay -> Int
 minutesDiff (TimeOfDay t2) (TimeOfDay t1) = t2 - t1
+
+-- Format a 'TimeOfDay' for pretty-printing.
+ppTimeOfDay :: TimeOfDay -> Text
+ppTimeOfDay (TimeOfDay t) = ppInt hours <> ":" <> ppInt mins
+  where (hours, mins) = t `divMod` 60
 
 
 -- | A period between two 'TimeOfDay's.  Valid iff the first value is less than
@@ -43,6 +59,10 @@ makeInterval (TimeOfDay t1) (TimeOfDay t2)
 duration :: Interval -> Int
 duration (Interval t1 t2) = t2 `minutesDiff` t1
 
+-- | Format an 'Interval' for pretty-printing.
+ppInterval :: Interval -> Text
+ppInterval (Interval tod1 tod2) = ppTimeOfDay tod1 <> "  " <> ppTimeOfDay tod2
+
 
 -- A description of activities undertaken in a working day.
 data Work = Work [Interval]
@@ -51,6 +71,37 @@ data Work = Work [Interval]
           | Leave
           deriving (Eq, Ord, Show)
 
+-- | Calculate the total minutes worked.
+workMinutes :: Work -> Int
+workMinutes (Work intvls) = sum (map duration intvls)
+workMinutes TOIL          = 0
+workMinutes Training      = 0
+workMinutes Leave         = 0
+
+-- | Minutes scheduled for this type of work day.
+scheduledMinutes :: Work -> Int
+scheduledMinutes (Work _) = regulationMinutes
+scheduledMinutes TOIL     = regulationMinutes
+scheduledMinutes Training = 0
+scheduledMinutes Leave    = 0
+
+-- | Calculate the balance of minutes worked versus scheduled.
+workBalance :: Work -> Int
+workBalance w = workMinutes w - scheduledMinutes w
+
+-- | Format a 'Work' for pretty-printing.
+ppWork :: Work -> Text
+ppWork w@(Work intvls) = T.pack $ printf "%-54s  |  %.2f (%2d:%02d) [%6.2f]" inOuts hDec hours mins balHDec
+  where
+    inOuts = T.intercalate "  " $ map ppInterval intvls
+    hDec = (fromIntegral (workMinutes w)) / 60.0 :: Double
+    (hours, mins) = (workMinutes w) `divMod` 60
+    balHDec = (fromIntegral (workBalance w)) / 60.0 :: Double
+ppWork TOIL          = "TOIL"
+ppWork Training      = "Training"
+ppWork Leave         = "Leave"
+
+
 -- | A complete record of a working day.
 data DayRecord = DayRecord { drDay     :: Day
                            , drWork    :: Work
@@ -58,12 +109,9 @@ data DayRecord = DayRecord { drDay     :: Day
                            } deriving (Eq, Ord, Show)
 
 -- | Calculate the balance of minutes worked versus scheduled.
-workBalance :: Work -> Int
-workBalance (Work intvls) = sum $ map duration intvls
-workBalance TOIL          = negate scheduledMinutes
-workBalance Training      = 0
-workBalance Leave         = 0
-
--- | Calculate the balance of minutes worked versus scheduled.
 dayBalance :: DayRecord -> Int
 dayBalance = workBalance . drWork
+
+-- | Format a 'DayRecord' for pretty-printing.
+ppDayRecord :: DayRecord -> Text
+ppDayRecord DayRecord{..} = T.pack (show drDay) <> "  |  " <> ppWork drWork <> maybe "" ("  " <>) drComment
